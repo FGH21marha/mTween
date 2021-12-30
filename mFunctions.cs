83e5791db633b9c2368c8f3056a11194221177c6
Martin <martin.halldin@futuregames.nu>
2021-12-30 13:35:53 +0100

    moved around Functions!

--- mFunctions.cs
+++ mFunctions.cs
@@ -1,8 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public partial class mTimeline
 {
+    /// <summary>
+    /// Rotates Transform to face a target
+    /// </summary>
+    public mTimeline RotateTowardsTarget(Transform follower, Transform Target)
+    {
+        Quaternion lookDir = Quaternion.FromToRotation(follower.forward, Target.position - follower.position);
+        lerpRot.Add(new RotationLerp((i) => follower.rotation = i, follower.rotation, lookDir));
+        return this;
+    }
+    public mTimeline RotateTowardsTarget(Transform follower, Transform Target, AnimationCurve curve)
+    {
+        Quaternion lookDir = Quaternion.FromToRotation(follower.forward, Target.position - follower.position);
+        lerpRot.Add(new RotationLerp((i) => follower.rotation = i, follower.rotation, lookDir, curve));
+        return this;
+    }
 
+    /// <summary>
+    /// Make a transform face a target transform continiously (instant)
+    /// </summary>
+    public mTimeline FaceTargetContinuous(Transform t, Transform target)
+    {
+        lerpTarget.Add(new ContinuousTargetLerp(t, target));
+        return this;
+    }
+    public mTimeline FaceTargetContinuous(Transform t, Transform target, Vector3 worldUp)
+    {
+        lerpTarget.Add(new ContinuousTargetLerp(t, target, worldUp));
+        return this;
+    }
 }
--- mTimeline.cs
+++ mTimeline.cs
@@ -557,36 +557,6 @@ using System.Collections.Generic;
         return this;
     }
 
-    /// <summary>
-    /// Rotates Transform to face a target
-    /// </summary>
-    public mTimeline RotateTowardsTarget(Transform follower, Transform Target)
-    {
-        Quaternion lookDir = Quaternion.FromToRotation(follower.forward, Target.position - follower.position);
-        lerpRot.Add(new RotationLerp((i) => follower.rotation = i, follower.rotation, lookDir));
-        return this;
-    }
-    public mTimeline RotateTowardsTarget(Transform follower, Transform Target, AnimationCurve curve)
-    {
-        Quaternion lookDir = Quaternion.FromToRotation(follower.forward, Target.position - follower.position);
-        lerpRot.Add(new RotationLerp((i) => follower.rotation = i, follower.rotation, lookDir, curve));
-        return this;
-    }
-
-    /// <summary>
-    /// Make a transform face a target transform continiously (instant)
-    /// </summary>
-    public mTimeline FaceTargetContinuous(Transform t, Transform target)
-    {
-        lerpTarget.Add(new ContinuousTargetLerp(t, target));
-        return this;
-    }
-    public mTimeline FaceTargetContinuous(Transform t, Transform target, Vector3 worldUp)
-    {
-        lerpTarget.Add(new ContinuousTargetLerp(t, target, worldUp));
-        return this;
-    }
-
     protected struct Vector2Lerp
     {
         public Action<Vector2> a;
