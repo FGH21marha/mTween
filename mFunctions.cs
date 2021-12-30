a4b906cb66fa78029a49cfea0b989649cf0d9c83
Martin <martin.halldin@futuregames.nu>
2021-12-30 14:38:25 +0100

    method overloaaaaaad

--- mFunctions.cs
+++ mFunctions.cs
@@ -59,6 +59,27 @@ public partial class mTimeline
         lerpVector3.Add(new Vector3Lerp((i) => t.position = i, from, to, curve));
         return this;
     }
+    public mTimeline MoveTo(Transform t, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.position = i, t.position, to));
+        return this;
+    }
+    public mTimeline MoveTo(Transform t, Vector2 from, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.position = i, from, to));
+        return this;
+    }
+    public mTimeline MoveTo(Transform t, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.position = i, t.position, to, curve));
+        return this;
+    }
+    public mTimeline MoveTo(Transform t, Vector2 from, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.position = i, from, to, curve));
+        return this;
+    }
+
     /// <summary>
     /// Moves transform towards position in local space
     /// </summary>
@@ -80,6 +101,314 @@ public partial class mTimeline
     public mTimeline MoveToLocal(Transform t, Vector3 from, Vector3 to, AnimationCurve curve)
     {
         lerpVector3.Add(new Vector3Lerp((i) => t.localPosition = i, from, to, curve));
+        return this;
+    }
+    public mTimeline MoveToLocal(Transform t, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localPosition = i, t.localPosition, to));
+        return this;
+    }
+    public mTimeline MoveToLocal(Transform t, Vector2 from, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localPosition = i, from, to));
+        return this;
+    }
+    public mTimeline MoveToLocal(Transform t, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localPosition = i, t.localPosition, to, curve));
+        return this;
+    }
+    public mTimeline MoveToLocal(Transform t, Vector2 from, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localPosition = i, from, to, curve));
+        return this;
+    }
+
+    /// <summary>
+    /// Scales Transform towards size
+    /// </summary>
+    public mTimeline ScaleTo(Transform t, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localScale = i, t.localScale, to));
+        return this;
+    }
+    public mTimeline ScaleTo(Transform t, Vector2 from, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localScale = i, from, to));
+        return this;
+    }
+    public mTimeline ScaleTo(Transform t, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localScale = i, t.localScale, to, curve));
+        return this;
+    }
+    public mTimeline ScaleTo(Transform t, Vector2 from, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.localScale = i, from, to, curve));
+        return this;
+    }
+    public mTimeline ScaleTo(Transform t, Vector3 to)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.localScale = i, t.localScale, to));
+        return this;
+    }
+    public mTimeline ScaleTo(Transform t, Vector3 from, Vector3 to)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.localScale = i, from, to));
+        return this;
+    }
+    public mTimeline ScaleTo(Transform t, Vector3 to, AnimationCurve curve)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.localScale = i, t.localScale, to, curve));
+        return this;
+    }
+    public mTimeline ScaleTo(Transform t, Vector3 from, Vector3 to, AnimationCurve curve)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.localScale = i, from, to, curve));
+        return this;
+    }
+
+    /// <summary>
+    /// Rotates Transform towards rotation
+    /// </summary>
+    public mTimeline RotateTo(Transform t, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, to));
+        return this;
+    }
+    public mTimeline RotateTo(Transform t, Quaternion from, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, from, to));
+        return this;
+    }
+    public mTimeline RotateTo(Transform t, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, to, curve));
+        return this;
+    }
+    public mTimeline RotateTo(Transform t, Quaternion from, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, from, to, curve));
+        return this;
+    }
+    public mTimeline RotateTo(Transform t, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateTo(Transform t, Vector3 from, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, Quaternion.Euler(from), Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateTo(Transform t, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, Quaternion.Euler(to), curve));
+        return this;
+    }
+    public mTimeline RotateTo(Transform t, Vector3 from, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, Quaternion.Euler(from), Quaternion.Euler(to), curve));
+        return this;
+    }
+
+    /// <summary>
+    /// Rotates Transform towards rotation in local space
+    /// </summary>
+    public mTimeline RotateToLocal(Transform t, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, to));
+        return this;
+    }
+    public mTimeline RotateToLocal(Transform t, Quaternion from, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, from, to));
+        return this;
+    }
+    public mTimeline RotateToLocal(Transform t, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, to, curve));
+        return this;
+    }
+    public mTimeline RotateToLocal(Transform t, Quaternion from, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, from, to, curve));
+        return this;
+    }
+    public mTimeline RotateToLocal(Transform t, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateToLocal(Transform t, Vector3 from, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, Quaternion.Euler(from), Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateToLocal(Transform t, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, Quaternion.Euler(to), curve));
+        return this;
+    }
+    public mTimeline RotateToLocal(Transform t, Vector3 from, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, Quaternion.Euler(from), Quaternion.Euler(to), curve));
+        return this;
+    }
+
+    /// <summary>
+    /// Moves RectTransform towards position
+    /// </summary>
+    public mTimeline MoveTo(RectTransform t, Vector3 to)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.anchoredPosition = i, t.anchoredPosition, to));
+        return this;
+    }
+    public mTimeline MoveTo(RectTransform t, Vector3 from, Vector3 to)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.anchoredPosition = i, from, to));
+        return this;
+    }
+    public mTimeline MoveTo(RectTransform t, Vector3 to, AnimationCurve curve)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.anchoredPosition = i, t.anchoredPosition, to, curve));
+        return this;
+    }
+    public mTimeline MoveTo(RectTransform t, Vector3 from, Vector3 to, AnimationCurve curve)
+    {
+        lerpVector3.Add(new Vector3Lerp((i) => t.anchoredPosition = i, from, to, curve));
+        return this;
+    }
+    public mTimeline MoveTo(RectTransform t, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.anchoredPosition = i, t.anchoredPosition, to));
+        return this;
+    }
+    public mTimeline MoveTo(RectTransform t, Vector2 from, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.anchoredPosition = i, from, to));
+        return this;
+    }
+    public mTimeline MoveTo(RectTransform t, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.anchoredPosition = i, t.anchoredPosition, to, curve));
+        return this;
+    }
+    public mTimeline MoveTo(RectTransform t, Vector2 from, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.anchoredPosition = i, from, to, curve));
+        return this;
+    }
+
+    /// <summary>
+    /// Scales RectTransform towards size
+    /// </summary>
+    public mTimeline ScaleTo(RectTransform t, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.sizeDelta = i, t.sizeDelta, to));
+        return this;
+    }
+    public mTimeline ScaleTo(RectTransform t, Vector2 from, Vector2 to)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.sizeDelta = i, from, to));
+        return this;
+    }
+    public mTimeline ScaleTo(RectTransform t, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.sizeDelta = i, t.sizeDelta, to, curve));
+        return this;
+    }
+    public mTimeline ScaleTo(RectTransform t, Vector2 from, Vector2 to, AnimationCurve curve)
+    {
+        lerpVector2.Add(new Vector2Lerp((i) => t.sizeDelta = i, from, to, curve));
+        return this;
+    }
+
+    /// <summary>
+    /// Rotates RectTransform towards rotation
+    /// </summary>
+    public mTimeline RotateTo(RectTransform t, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, to));
+        return this;
+    }
+    public mTimeline RotateTo(RectTransform t, Quaternion from, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, from, to));
+        return this;
+    }
+    public mTimeline RotateTo(RectTransform t, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, to, curve));
+        return this;
+    }
+    public mTimeline RotateTo(RectTransform t, Quaternion from, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, from, to, curve));
+        return this;
+    }
+    public mTimeline RotateTo(RectTransform t, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateTo(RectTransform t, Vector3 from, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, Quaternion.Euler(from), Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateTo(RectTransform t, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, t.rotation, Quaternion.Euler(to), curve));
+        return this;
+    }
+    public mTimeline RotateTo(RectTransform t, Vector3 from, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.rotation = i, Quaternion.Euler(from), Quaternion.Euler(to), curve));
+        return this;
+    }
+
+    /// <summary>
+    /// Rotates RectTransform towards rotation in local space
+    /// </summary>
+    public mTimeline RotateToLocal(RectTransform t, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, to));
+        return this;
+    }
+    public mTimeline RotateToLocal(RectTransform t, Quaternion from, Quaternion to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, from, to));
+        return this;
+    }
+    public mTimeline RotateToLocal(RectTransform t, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, to, curve));
+        return this;
+    }
+    public mTimeline RotateToLocal(RectTransform t, Quaternion from, Quaternion to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, from, to, curve));
+        return this;
+    }
+    public mTimeline RotateToLocal(RectTransform t, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateToLocal(RectTransform t, Vector3 from, Vector3 to)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, Quaternion.Euler(from), Quaternion.Euler(to)));
+        return this;
+    }
+    public mTimeline RotateToLocal(RectTransform t, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, t.localRotation, Quaternion.Euler(to), curve));
+        return this;
+    }
+    public mTimeline RotateToLocal(RectTransform t, Vector3 from, Vector3 to, AnimationCurve curve)
+    {
+        lerpRot.Add(new RotationLerp((i) => t.localRotation = i, Quaternion.Euler(from), Quaternion.Euler(to), curve));
         return this;
     }
 }
